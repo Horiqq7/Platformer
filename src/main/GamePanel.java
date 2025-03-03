8e50c2a48146f9b7b45d3d91cfa281631c2fa5bd
Horia Rusu <horiarusu612@gmail.com>
2025-03-03 23:23:22 +0200

    First commit, basic mechanics of the game. Created a game loop, built animations for running, idle and attacking.

---
+++ src/main/GamePanel.java
@@ -0,0 +1,54 @@
+package main;
+
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static utils.Constants.PlayerConstants.*;
+import static utils.Constants.Directions.*;
+
+public class GamePanel extends JPanel {
+    private MouseInputs mouseInputs;
+    private Game game;
+
+
+    public GamePanel(Game game) {
+        mouseInputs = new MouseInputs(this);
+        this.game = game;
+
+        setPanelSize();
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+
+        setDoubleBuffered(true);
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 800);
+        setMinimumSize(size);
+        setMaximumSize(size);
+        setPreferredSize(size);
+    }
+
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        game.render(g);
+    }
+
+
+    public void updateGame() {
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+}
